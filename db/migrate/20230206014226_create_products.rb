7c6a8c91514bb8b96df2014cedf67ee282c0df7f
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2023-02-06 10:53:23 +0900

    マイグレーションファイルの作成

--- db/migrate/20230206014226_create_products.rb
+++ db/migrate/20230206014226_create_products.rb
@@ -1,7 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-
+      t.string  :title, null: false
+      t.integer :price, null: false
       t.timestamps
     end
   end
